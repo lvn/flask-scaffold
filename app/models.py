d436141e27a303e698b59f97d47a891e9dbcdaac
Elvin Yung <elvinyung1@gmail.com>
2014-06-03 21:56:18 -0400

    user model

--- app/models.py
+++ app/models.py
@@ -2,3 +2,8 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from app import db
 
 # Models go here.
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64))
+	password = db.String(db.String(64))
+	email = db.String(db.String(256))
--- app/views.py
+++ app/views.py
@@ -6,3 +6,17 @@ from app import app, db, lm
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+	if (request.method == 'POST'):
+		# register new account models
+		return
+	return render_template('register.html', title="Register your account")
+
+@app.route('/login/', methods=['GET','POST'])
+def login():
+	if (request.method == 'POST'):
+		# login account
+		return
+	return render_template('login.html', title="Login")
